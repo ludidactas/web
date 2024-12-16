3ad1a1d3468ab41eb96256f0f9a456523db8b1a9
VladimirCharkot <vlad.chk@gmail.com>
2024-12-15 23:52:40 -0300

    feature: página para levantar los md

--- app/a/[slug]/page.tsx
+++ app/a/[slug]/page.tsx
@@ -1,4 +1,18 @@
+import { getArticulo } from "@/md";
+
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const slug = (await params).slug
-  return <div>Page {slug}</div>;
+
+  // Si tenemos a este artículo listado, traemos su MD
+  const [Componente, meta] = getArticulo(slug)
+
+  return <>
+  {/* Si hay componente lo rendereamos */}
+  {Componente && <Componente />}
+
+  {/* Si no, mostramos un mensaje */}
+  {!Componente && <p className="p-8">
+    No tenemos indexado el componente `{slug}`. O bien falta el MD en la carpeta `md`, o bien falta indexarlo en `app/a/[slug]/page.tsx`
+    </p>}
+  </>;
 }
---
+++ md/index.ts
@@ -0,0 +1,21 @@
+// Índice de markdowns
+
+// app/a/[slug]/page.tsx
+import Math, { meta } from '@/md/matematica.mdx'
+import { MDXProps } from 'mdx/types'
+
+// Los valores de este enum tienen que matchear los ids que vengan de affinity/obsidian
+enum Articulo{
+  Math = 'math'
+}
+
+const articulos: Record<Articulo, [React.ComponentType<MDXProps>, Record<string, any>]> = {
+  [Articulo.Math] : [Math, meta]
+}
+
+/** 
+ * Devuelve un artículo y su `meta`, dado un id, o `[null, null]` si el id no existe
+ */
+export const getArticulo = (id: string) => Object.keys(articulos).includes(id) ? articulos[id as Articulo] : [null, null]
+
+export default articulos

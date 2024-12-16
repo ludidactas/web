f6cf79df805357d5faf07e08687ff664d2d901ab
VladimirCharkot <vlad.chk@gmail.com>
2024-12-15 23:51:54 -0300

    refactor: muevo roadmap a su propio componente

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,12 @@
-'use client'
-import { useEffect, useRef, useState } from 'react'
-import Roadmap from './roadmap.svg'
-// import { useRouter } from 'next/navigation'
-import { ModeToggle } from '@/components/ui/mode-toggle'
-import MdDrawer from '@/components/custom/md-drawer'
+import Link from 'next/link'
 
 export default function Home() {
-  // const router = useRouter()
-  const svgRef = useRef<SVGAElement>()
-  const [articulo, setArticulo] = useState<null | string>(null)
-  const [isOpen, setIsOpen] = useState(false)
-
-  // Targeteamos los elementos con id que empiecen con 'rm.' y suscribimos eventos
-  useEffect(() => {
-    if (svgRef.current) {
-      const elementos_con_id = svgRef.current.querySelectorAll('[id*="rm."]')
-      elementos_con_id.forEach((elemento) => {
-        const id = elemento.id.split('.')[1]
-        elemento.addEventListener('click', () => {
-          console.log(`click en ${id}`)
-          setArticulo(id)
-          setIsOpen(true)
-          // router.push(`/a/${id}`)
-        })
-        elemento.addEventListener('mouseenter', () => {
-          console.log(`mouseenter en ${id}`)
-          elemento.classList.add('activo')
-        })
-        elemento.addEventListener('mouseleave', () => {
-          console.log(`mouseleave en ${id}`)
-          elemento.classList.remove('activo')
-        })
-      })
-    }
-  }, [svgRef])
-
   return (
     <div className="p-8">
-      <h1>POC Roadmaps</h1>
-      <ModeToggle />
-      {articulo && <MdDrawer articulo={articulo} isOpen={isOpen} setIsOpen={setIsOpen} />}
-      <Roadmap ref={svgRef} />
+      <Link href="/roadmap">Roadmap</Link>
+
+      {/* Contenido pendiente */}
+
     </div>
   )
 }
---
+++ app/roadmap/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+import RoadmapSvg from '@/app/roadmap.svg'
+import { useEffect, useRef, useState } from 'react'
+import RoadmapDrawer from '@/components/custom/ld-drawer'
+
+export default function Roadmap() {
+
+  // Ref para el svg
+  const svgRef = useRef<SVGAElement>()
+
+  // Nombre del artículo que se halla clickeado
+  const [articulo, setArticulo] = useState<null | string>(null)
+
+  // Estado del drawer
+  const [isCajonAbierto, setIsCajonAbierto] = useState(false)
+
+  // Targeteamos los elementos con id que empiecen con 'rm.' y suscribimos eventos
+  useEffect(() => {
+    if (svgRef.current) {
+
+      // Recogemos todos los elementos svg que tengan un id que comienza con "rm."
+      const elementos_con_id = svgRef.current.querySelectorAll('[id*="rm."]')
+
+      // Para cada uno
+      elementos_con_id.forEach((elemento) => {
+
+        // Obtenemos la parte del id que venga después del punto
+        const id = elemento.id.split('.')[1]
+
+        // Al hacerle click, abrimos el cajón y le pasamos este id
+        elemento.addEventListener('click', () => {
+          console.log(`click en ${id}`)
+          setArticulo(id)
+          setIsCajonAbierto(true)
+        })
+
+        // Al entrar, le aplicamos la clase "activo"
+        elemento.addEventListener('mouseenter', () => {
+          console.log(`mouseenter en ${id}`)
+          elemento.classList.add('activo')
+        })
+
+        // Al salir, se la quitamos
+        elemento.addEventListener('mouseleave', () => {
+          console.log(`mouseleave en ${id}`)
+          elemento.classList.remove('activo')
+        })
+      })
+    }
+  }, [svgRef])
+
+  return (
+    <div className="p-8">
+      
+      {/* Si `articulo` está definida, renderizamos el drawer */}
+      {articulo && <RoadmapDrawer articulo={articulo} isOpen={isCajonAbierto} setIsOpen={setIsCajonAbierto} />}
+      <RoadmapSvg ref={svgRef} />
+    </div>
+  )
+}

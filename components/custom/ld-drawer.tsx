2372904b5524df0e63acbe2797c34d9ee26b3b65
Vlad <vlad.chk@gmail.com>
2024-12-27 01:27:41 -0300

    setup: cinstalación de recharts y componentes de shadcn card y chart

--- components/custom/ld-drawer.tsx
+++ components/custom/ld-drawer.tsx
@@ -15,36 +15,35 @@ const LdDrawer = ({
   isOpen: boolean
   setIsOpen: Dispatch<SetStateAction<boolean>>
 }) => {
-
   // Importamos el router
   const router = useRouter()
 
-  const [_, meta] = getArticulo(articulo) 
-
-  console.log(`@LdDrawer meta:`, meta)
+  const [_, meta] = getArticulo(articulo)
 
   return (
-  <Drawer.Root open={isOpen} onOpenChange={setIsOpen}>
-    <DrawerContent>
-      <DrawerHeader>
-        <DrawerTitle>{capitalize(articulo)}</DrawerTitle>
-        <DrawerDescription>
-          {meta?.descripcion ?? `Acá iría la pequeña descripción de ${articulo}, pero no está. Agregarla al front-matter del MD en cuestión con la clave 'descripcion'.`}
-        </DrawerDescription>
-      </DrawerHeader>
-      <DrawerFooter>
-        
-        {/* Navegar a la página del artículo */}
-        <Button disabled={!meta?.descripcion} onClick={() => router.push(`/a/${articulo}`)}>Acceder</Button>
+    <Drawer.Root open={isOpen} onOpenChange={setIsOpen}>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>{capitalize(articulo)}</DrawerTitle>
+          <DrawerDescription>
+            {meta?.descripcion ??
+              `Acá iría la pequeña descripción de ${articulo}, pero no está. Agregarla al front-matter del MD en cuestión con la clave 'descripcion'.`}
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          {/* Navegar a la página del artículo */}
+          <Button disabled={!meta?.descripcion} onClick={() => router.push(`/a/${articulo}`)}>
+            Acceder
+          </Button>
 
-        {/* Cerrar */}
-        <Button onClick={() => setIsOpen(false)} variant="outline">
-          Cerrar
-        </Button>
-
-      </DrawerFooter>
-    </DrawerContent>
-  </Drawer.Root>
-)}
+          {/* Cerrar */}
+          <Button onClick={() => setIsOpen(false)} variant="outline">
+            Cerrar
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer.Root>
+  )
+}
 
 export default LdDrawer

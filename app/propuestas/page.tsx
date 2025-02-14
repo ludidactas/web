bfae455508859ea4ae7ff8dfb826316ec0f01802
Alejandra Correa <comomaraleja@gmail.com>
2025-02-14 11:03:54 -0300

    content:dev: carga de contenido y estilos de componente "propuestas"

---
+++ app/propuestas/page.tsx
@@ -0,0 +1,5 @@
+import PropuestasComp from "./propuestasComp"
+
+export default function Page(){
+    return <PropuestasComp/>
+}
---
+++ app/propuestas/propuestasComp.tsx
@@ -0,0 +1,7 @@
+import PropCont from "@/app/propuestas/propuestasCont.mdx"
+
+export default function propuestasComp(){
+    return <div className="propuestas">
+        <PropCont/>
+        </div>
+}

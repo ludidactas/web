8a24491bed26499586edc9d7a5912c6be9c4975f
Vlad <vlad.chk@gmail.com>
2025-03-01 10:50:50 -0300

    fix: quitamos GA y GTM hasta que estén en el dominio correcto

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import './md.css'
 
 import { BibliotecaRoot } from '@/components/contenido-provider'
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
+// import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import Menu from './inicio/menu'
 import LogoLema from './inicio/logoLema'
 import { inter, NovaF } from '@/components/fonts'
@@ -21,8 +21,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     // Warning: suppressHydrationWarning está porque https://github.com/shadcn-ui/ui/issues/5552
     <html lang="en">
       <body className={`${NovaF.className} ${inter.className} antialiased`}>
-        <GoogleAnalytics gaId="G-VYSMHGH9RZ" />
-        <GoogleTagManager gtmId="GTM-M4H5VXKB" />
+        {/* <GoogleAnalytics gaId="G-VYSMHGH9RZ" /> */}
+        {/* <GoogleTagManager gtmId="GTM-M4H5VXKB" /> */}
         <BibliotecaRoot>
           <Textura>
             <div className="flex dark:bg-[#1e1e1e] justify-between p-2 px-4">
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               <Menu />
             </div>
             {children}
-            <Footer/>
+            <Footer />
           </Textura>
         </BibliotecaRoot>
       </body>

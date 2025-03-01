2e6e6082292b1a85d03fecce69794f984089cc83
Vlad <vlad.chk@gmail.com>
2025-03-01 10:45:44 -0300

    fix: cargamos p5 lazy de manera que no se intente importar en el server

--- components/fx/textura.tsx
+++ components/fx/textura.tsx
@@ -1,18 +1,26 @@
 'use client'
-import React, { PropsWithChildren, createRef, useEffect } from 'react'
-import p5 from 'p5'
+import React, { PropsWithChildren, createRef, useEffect, useState } from 'react'
 import sketch from './sketch'
 
-// const Textura = ({children}: React.PropsWithChildren) => <div className="bg-textura bg-cover">{children}</div>
 const Textura = ({ children }: PropsWithChildren) => {
   const canvasContainer = createRef<HTMLDivElement>()
+  const [p5, setP5] = useState<any>()
+
+  // Cargamos dinámicamente p5 al cargar el componente - client side _only_
+  useEffect(() => {
+    import('p5').then((p5Module) => {
+      setP5(p5Module.default)
+    })
+  }, [])
 
   useEffect(() => {
-    const myp5 = new p5(sketch, canvasContainer.current!)
-    return () => {
-      myp5.remove()
+    if (p5) {
+      const myp5 = new p5(sketch, canvasContainer.current!)
+      return () => {
+        myp5.remove()
+      }
     }
-  }, [canvasContainer])
+  }, [p5, canvasContainer])
 
   return (
     <div>

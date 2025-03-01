a51b676acb4ab84ae9f8b7d134729a2e52b8b62a
Vlad <vlad.chk@gmail.com>
2025-02-28 23:06:44 -0300

    refactor: quito las cosas del roadmap y las otras weas de main

--- app/inicio/recursos.tsx
+++ app/inicio/recursos.tsx
@@ -1,12 +1,27 @@
 import Pantalla from './pantalla'
 import Image from 'next/image'
-import Recur, {meta} from '@/app/inicio/recursos.mdx'
+//@ts-ignore meta
+import Recur, { meta } from '@/app/inicio/recursos.mdx'
 import Link from 'next/link'
 
 export default function Recursos() {
-  return <Pantalla one={<Recur/>} two={<Imagenes/>} title={meta.titulo} btn={<Link className="custom-btn btn-15" href="/roadmap"> Explorar recursos </Link>} />
+  return (
+    <Pantalla
+      one={<Recur />}
+      two={<Imagenes />}
+      title={meta.titulo}
+      btn={
+        <Link className="custom-btn btn-15" href="/roadmap">
+          {' '}
+          Explorar recursos{' '}
+        </Link>
+      }
+    />
+  )
 }
 
-const Imagenes = () => <div>
-  <Image src="/img/PersoRecursos.png" alt="Personaje3" width={500} height={500}/>
+const Imagenes = () => (
+  <div>
+    <Image src="/img/PersoRecursos.png" alt="Personaje3" width={500} height={500} />
   </div>
+)

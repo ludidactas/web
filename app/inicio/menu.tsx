8f8e06a0de8c4a51f7d969d51f7023b04b20dbc0
Vlad <vlad.chk@gmail.com>
2025-02-28 21:34:56 -0300

    dev: arreglitos al menu

--- app/inicio/menu.tsx
+++ app/inicio/menu.tsx
@@ -1,37 +1,41 @@
-// "use client"
-// import { AlignJustify } from "lucide-react";
-// import React, { useState } from "react";
-// import IdentidadComp from "../identidad/IdentidadComp";
-// import Link from "next/link";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { AlignJustify } from "lucide-react";
-import Link from "next/link";
-import { ComponentProps } from "react";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { AlignJustify } from 'lucide-react'
+import Link from 'next/link'
+import { ComponentProps } from 'react'
 
-
-const MenuMobile = () =>
-    <DropdownMenu>
-        <DropdownMenuTrigger className="block md:hidden">
-            <AlignJustify size={44} />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuLabel>MENU</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Identidad</DropdownMenuItem>
-            <DropdownMenuItem>Propuestas</DropdownMenuItem>
-            {/* <DropdownMenuItem>Recursos</DropdownMenuItem> */}
-            <DropdownMenuItem>Contacto</DropdownMenuItem>
-        </DropdownMenuContent>
-    </DropdownMenu>
-
-
+const MenuMobile = () => (
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <AlignJustify size={44} />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuLabel>MENU</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>
+        <Link href="/propuestas">Identidad</Link>
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        <Link href="/identidad">Propuestas</Link>
+      </DropdownMenuItem>
+      {/* <DropdownMenuItem>
+        <Link href="https://www.instagram.com/recursos/">Recursos</Link>
+      </DropdownMenuItem> */}
+      <DropdownMenuItem>
+        <Link href="https://www.instagram.com/ludidactas/">Contacto</Link>
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        <Link href="https://ludidactas.medium.com/">Blog</Link>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+)
 
 // const [isOpen, setIsOpen] = useState(false);
 
@@ -69,29 +73,40 @@ const MenuMobile = () =>
 //     </div>
 // );
 
-
-
 // Version props explícitos
 // interface MenuLinkEjemploProps{ href: string, children: ReactNode }
 // const MenuLinkEjemplo = ({ href, children }: MenuLinkEjemploProps) => <Link className="todoloquequiera" href={href}>{children}</Link>
 
 //version props derivados existentes en Link
-const MenuLink = (props: ComponentProps<typeof Link>) => <Link {...props} className="border-2 rounded-[10px] p-2 hover:bg-slate-200" />
+const MenuLink = (props: ComponentProps<typeof Link>) => (
+  <Link {...props} className="border-2 rounded-[10px] p-2 hover:bg-slate-200" />
+)
 
-const MenuDesktop = () => <div className="flex gap-4 items-center p-4">
+const MenuDesktop = () => (
+  <div className="flex gap-4 items-center p-4">
     {/* Implementacion version props */}
     <MenuLink href="/identidad"> Identidad </MenuLink>
     <MenuLink href="/propuestas"> Propuestas </MenuLink>
     {/* <MenuLink href="/roadmap"> Recursos</MenuLink> */}
-    <MenuLink target="_blank" href="https://www.instagram.com/ludidactas/"> Contacto </MenuLink>
-</div>
+    <MenuLink target="_blank" href="https://www.instagram.com/ludidactas/">
+      Contacto
+    </MenuLink>
+    <MenuLink target="_blank" href="https://ludidactas.medium.com/">
+      Contacto
+    </MenuLink>
+  </div>
+)
 
 //Renderiza uno u otro según se le indique en
-const Menu = () =>
-    <>
-        <MenuMobile />
-        <MenuDesktop />
-    </>
+const Menu = () => (
+  <>
+    <div className="block md:hidden">
+      <MenuMobile />
+    </div>
+    <div className="hidden md:block">
+      <MenuDesktop />
+    </div>
+  </>
+)
 
-
-export default Menu;
+export default Menu

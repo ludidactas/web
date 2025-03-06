8b6dc28bc5d68d4fca3007d3f835d552d702467e
Alejandra <comomaraleja@gmail.com>
2025-03-06 17:02:32 -0300

    dev:carousel loopeante

--- components/ui/ld-carousel.tsx
+++ components/ui/ld-carousel.tsx
@@ -61,6 +61,7 @@ const Carousel = React.forwardRef<
     const [carouselRef, api] = useEmblaCarousel(
       {
         ...opts,
+        loop:true,
         axis: orientation === "horizontal" ? "x" : "y",
       },
       plugins

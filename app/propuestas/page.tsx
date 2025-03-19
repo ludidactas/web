b0be6d5452ecb3473a829aaa8b0b0642bfcf45e7
Alejandra <comomaraleja@gmail.com>
2025-03-19 16:46:27 -0300

    style: ajustes para deployment

--- app/propuestas/page.tsx
+++ app/propuestas/page.tsx
@@ -159,17 +159,20 @@ const LogoTec = ({ nombre, url, descripcion }: { nombre: string; url: string; de
 
   return (
     <TooltipProvider delayDuration={0}> {/* Disables hover delay */}
-      <Tooltip open={open} onOpenChange={setOpen}>
-        <TooltipTrigger
-          onClick={() => setOpen(!open)}>
-          <Image className="w-fit h-fit md:w-full md:h-full " src={url} width={100} height={100} alt="" />
-        </TooltipTrigger>
+    <Tooltip open={open} onOpenChange={setOpen}>
+      <TooltipTrigger
+        onClick={() => setOpen(!open)}>
+        <Image className="w-fit h-fit md:w-full md:h-full " src={url} width={100} height={100} alt="" />
+      </TooltipTrigger>
         <TooltipContent className="bg-black text-center text-white w-[20em] p-5">
           <h1 className="text-2xl pb-2 text-[#4198AA]">{nombre}</h1>
           <p className='text-center'>{descripcion}</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
+
+   
+
   )
 }
 const Tecnologias = () => {

e92bbf14ea8a390987e0b31c9a3ae5f053cf9ced
Vlad <vlad.chk@gmail.com>
2025-04-02 16:07:28 -0300

    style: refactorizacion boton neon

--- app/inicio/identidad.tsx
+++ app/inicio/identidad.tsx
@@ -3,39 +3,40 @@
 import Image from 'next/image'
 //@ts-ignore meta
 import { CircleChevronDown } from 'lucide-react'
-import Link from 'next/link'
 import { Link as Scroll } from 'react-scroll'
 import Pantalla from './pantalla'
+import BtnNeon from '@/components/custom/ld-btn-neon'
 
 export default function Identidad() {
   return (
-
-    <div className='identidadini'>
-
+    <div className="identidadini">
       <Pantalla
         title="¿Quiénes somos?"
         one={
-
           <>
-            <p>Ludidactas surge en el año 2020 como un <span className="text-[#46BFD7]">laboratorio didáctico</span> cuyo objetivo consiste en la creación de nuevas propuestas pedagógicas de formación docente
-              (herramientas y recursos para la enseñanza) y de instancias educativas abiertas a todo público (talleres, cursos, encuentros) que atiendan de manera flexible,
-              responsable y eficiente a las preguntas y problemáticas subyacentes a la enseñanza.</p>
-            <p className='mt-4'>Nuestras propuestas tienen un enfoque <span className="text-[#46BFD7]">interdisciplinar, lúdico y reflexivo</span> y se encuentran articuladas en torno técnicas y tecnologías tales como la programación,
-              animación, el desarrollo de videojuegos y las matemáticas.</p>
+            <p>
+              Ludidactas surge en el año 2020 como un <span className="text-[#46BFD7]">laboratorio didáctico</span> cuyo
+              objetivo consiste en la creación de nuevas propuestas pedagógicas de formación docente (herramientas y
+              recursos para la enseñanza) y de instancias educativas abiertas a todo público (talleres, cursos,
+              encuentros) que atiendan de manera flexible, responsable y eficiente a las preguntas y problemáticas
+              subyacentes a la enseñanza.
+            </p>
+            <p className="mt-4">
+              Nuestras propuestas tienen un enfoque{' '}
+              <span className="text-[#46BFD7]">interdisciplinar, lúdico y reflexivo</span> y se encuentran articuladas
+              en torno técnicas y tecnologías tales como la programación, animación, el desarrollo de videojuegos y las
+              matemáticas.
+            </p>
           </>
         }
         two={<Imagenes />}
-        btn={
-          <Link className="custom-btn btn-15 w-fit" href="/identidad">
-            {' '}
-            Más sobre el proyecto{' '}
-          </Link>
-        }
+        btn={<BtnNeon href="/identidad"> Más sobre el proyecto </BtnNeon>}
         scroll={
-          <Scroll to="propuestasini" smooth={true} duration={500}><CircleChevronDown className="w-full h-full" /></Scroll>
-
-        } />
-
+          <Scroll to="propuestasini" smooth={true} duration={500}>
+            <CircleChevronDown className="w-full h-full" />
+          </Scroll>
+        }
+      />
     </div>
   )
 }

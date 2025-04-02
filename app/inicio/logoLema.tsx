82c08cdf8b38d4c616975f3b692d58449fe25487
Vlad <vlad.chk@gmail.com>
2025-04-02 15:35:12 -0300

    style: ordenamos las fonts para usar nombres semánticos (body, titulo, boton, etc) y poder testar diferentes fuentes

--- app/inicio/logoLema.tsx
+++ app/inicio/logoLema.tsx
@@ -1,42 +1,45 @@
-import { Jersey } from "@/components/fonts"
-import Image from "next/image"
-import Link from "next/link"
+import { titulo } from '@/components/fonts'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const LogoLemaDesktop=()=> {
-
-    return<Link className='logolema' href="/inicio">
-        <div className="flex w-[48em] items-center">
-            <Image  src="/img/Logo.png" alt={""} width={180} height={180} />
-             <div className="font-medium text-[7px] sm:text-[12px] md:text-[14px] lg:text-[18px] pt-1 dark:text-slate-50 w-[60%]">
-               <Image className="" src="/img/Lema.png" alt={""} width={800} height={800}/>
-                <p className={`${Jersey.className} text-[1em] m-0`}>Tecnologías educativas emergentes </p>
-            </div>
+const LogoLemaDesktop = () => {
+  return (
+    <Link className="logolema" href="/inicio">
+      <div className="flex w-[48em] items-center">
+        <Image src="/img/Logo.png" alt={''} width={180} height={180} />
+        <div className="font-medium text-[7px] sm:text-[12px] md:text-[14px] lg:text-[18px] pt-1 dark:text-slate-50 w-[60%]">
+          <Image className="" src="/img/Lema.png" alt={''} width={800} height={800} />
+          <p className={`${titulo.className} text-[1em] m-0`}>Tecnologías educativas emergentes </p>
         </div>
+      </div>
     </Link>
+  )
 }
 
-const LogoLemaMobile=()=>{
-    return<Link className='logolema w-[60vw]' href='inicio'>
-        <div className="flex items-center">
-            <Image  className="w-10" src="/img/Logo.png" alt={""} width={100} height={100} />
-             <div className="font-medium text-[10px] md:text-[14px] lg:text-[18px] pt-1 dark:text-slate-50 w-[60%]">
-               <Image className="" src="/img/Lema.png" alt={""} width={200} height={200}/>
-                <p className={`${Jersey.className} m-0`}>Tecnologías educativas emergentes </p>
-            </div>
+const LogoLemaMobile = () => {
+  return (
+    <Link className="logolema w-[60vw]" href="inicio">
+      <div className="flex items-center">
+        <Image className="w-10" src="/img/Logo.png" alt={''} width={100} height={100} />
+        <div className="font-medium text-[10px] md:text-[14px] lg:text-[18px] pt-1 dark:text-slate-50 w-[60%]">
+          <Image className="" src="/img/Lema.png" alt={''} width={200} height={200} />
+          <p className={`${titulo.className} m-0`}>Tecnologías educativas emergentes </p>
         </div>
+      </div>
     </Link>
+  )
 }
-const LogoLema=()=>{
-    return(
-      <>
-        <div className="block md:hidden">
-          <LogoLemaMobile />
-        </div>
-        <div className='hidden md:block'>
-          <LogoLemaDesktop />
-        </div>
-      </>
-    )
+const LogoLema = () => {
+  return (
+    <>
+      <div className="block md:hidden">
+        <LogoLemaMobile />
+      </div>
+      <div className="hidden md:block">
+        <LogoLemaDesktop />
+      </div>
+    </>
+  )
 }
 
-export default LogoLema;
+export default LogoLema
--- components/fonts/index.ts
+++ components/fonts/index.ts
@@ -1,26 +1,39 @@
 import localFont from 'next/font/local'
-import { Inter, Nova_Flat, Press_Start_2P, Pixelify_Sans, Jersey_10} from 'next/font/google'
+import { Inter, Nova_Flat, Press_Start_2P, Pixelify_Sans, Jersey_10, Funnel_Sans, Lexend } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] })
 
-export const NovaF = Nova_Flat({
+export const novaF = Nova_Flat({
   subsets: ['latin'],
   display: 'swap',
   weight: '400',
 })
-export const Jersey=Jersey_10({
-  weight:'400',
-  subsets: ['latin']
+
+export const funnelSans = Funnel_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
 })
 
-export const Pixelify=Pixelify_Sans({
-  weight:'400',
-  subsets: ['latin']
+export const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
 })
 
-export const Press2P = Press_Start_2P({
-  weight:'400',
-  subsets: ['latin']
+export const jersey = Jersey_10({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export const pixelify = Pixelify_Sans({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export const press2P = Press_Start_2P({
+  weight: '400',
+  subsets: ['latin'],
 })
 
 export const geistSans = localFont({
@@ -34,3 +47,7 @@ export const geistMono = localFont({
   variable: '--font-geist-mono',
   weight: '100 900',
 })
+
+export const body = lexend
+export const titulo = jersey
+export const boton = titulo

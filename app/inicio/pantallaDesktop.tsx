da1c38deebf0768d7c433f6443eeb59e83589852
Alejandra <comomaraleja@gmail.com>
2025-05-06 10:15:51 -0500

    style: ajustes cajas de texto

--- app/inicio/pantallaDesktop.tsx
+++ app/inicio/pantallaDesktop.tsx
@@ -1,5 +1,6 @@
 import { titulo } from '@/components/fonts'
 import { PantallaProps } from './pantalla'
+import Image from 'next/image'
 
 const PantallaDesktop = ({ title, one, two, btn, scroll, espejado = false }: PantallaProps) => (
   <div className="hidden lg:flex w-[100vw] h-[100vh] mt-5 mb-20 flex-col items-center place-content-center">
@@ -10,18 +11,26 @@ const PantallaDesktop = ({ title, one, two, btn, scroll, espejado = false }: Pan
       {title}
     </h2>
 
+  
     <div className={`flex px-40 items-center mx-10 gap-20 ${espejado ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
     <div
   className="relative w-full h-fit text-xl text-center bg-white/60 overflow-hidden px-8 pt-10 pb-8"
-  style={{
-    backgroundImage: 'url("/img/Caja1.svg")',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: "auto",
-    backgroundPosition: 'center',
-  }}
-  data-aos={espejado ? 'fade-left' : 'fade-right'}
+  // style={{
+  //   backgroundImage: 'url("/img/Caja.gif")',
+  //   backgroundRepeat: 'no-repeat',
+  //   backgroundSize: "auto",
+  //   backgroundPosition: 'center',
+  // }}
+    data-aos={espejado ? 'fade-left' : 'fade-right'}
 >
-  
+  {/* Background image behind the content */}
+  <Image
+    src="/img/Caja.gif"
+    alt=""
+    className="absolute w-fit inset-0 z-0"
+    width={400}
+    height={400}
+  />
 
   {/* Foreground content */}
   <div className="relative z-10">

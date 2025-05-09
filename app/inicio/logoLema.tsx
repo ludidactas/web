9a616737df264541a325705804c4f898fab6e239
Aleja <vlad.chk@gmail.com>
2025-05-09 16:46:23 -0500

    dev: refactorizacion logolema y pantalla

--- app/inicio/logoLema.tsx
+++ app/inicio/logoLema.tsx
@@ -2,43 +2,18 @@ import { titulo } from '@/components/fonts'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const LogoLemaDesktop = () => {
-  return (
-    <Link className="logolema" href="/inicio">
-      <div className="flex w-[48em] items-center gap-4">
-        <Image className="w-24" src="/img/logo_sketchy.gif" alt={''} width={180} height={180} />
-        <div className="font-medium text-[7px] sm:text-[12px] md:text-[14px] lg:text-[18px] pt-1 dark:text-slate-50 w-[60%]">
-          <Image className="" src="/img/lema_sketchy.gif" alt={''} width={800} height={800} />
-          <p className={`${titulo.className} text-[1em] m-0`}>Educación emergente </p>
-        </div>
-      </div>
-    </Link>
-  )
-}
 
-const LogoLemaMobile = () => {
-  return (
-    <Link className="logolema w-[60vw]" href="inicio">
-      <div className="flex items-center gap-4">
-        <Image className="w-10" src="/img/logo_sketchy.gif" alt={''} width={100} height={100} />
-        <div className="font-medium text-[10px] md:text-[14px] lg:text-[18px] pt-1 dark:text-slate-50 w-[60%]">
-          <Image className="" src="/img/lema_sketchy.gif" alt={''} width={200} height={200} />
-          <p className={`${titulo.className} m-0 text-nowrap`}>Educación emergente </p>
-        </div>
-      </div>
-    </Link>
-  )
-}
 const LogoLema = () => {
   return (
-    <>
-      <div className="block lg:hidden">
-        <LogoLemaMobile />
+    <Link className="logolema w-[60vw]" href="/inicio">
+      <div className="flex md:w-[48em] items-center gap-4">
+        <Image className="w-10 md:w-24" src="/img/logo_sketchy.gif" alt={''} width={100} height={100} />
+        <div className="font-medium text-[7px] sm:text-[12px] md:text-[14px] lg:text-[18px] pt-1 dark:text-slate-50 w-[60%]">
+          <Image className="w-[200px] md:w-[800px]" src="/img/lema_sketchy.gif" alt={''} width={200} height={200} />
+          <p className={`${titulo.className} m-0 text-nowrap md:text-[1em]`}>Educación emergente </p>
+        </div>
       </div>
-      <div className="hidden lg:block">
-        <LogoLemaDesktop />
-      </div>
-    </>
+    </Link>
   )
 }
 

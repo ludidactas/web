13f80cc675312900d370119a0f8cc39441077ec6
Aleja <vlad.chk@gmail.com>
2025-05-14 13:49:50 -0500

    dev: prueba animation svg

---
+++ app/pruebas/page.tsx
@@ -0,0 +1,10 @@
+import SvgAnimator from "@/components/custom/ld-svgAnimation"
+
+export default function Page(){
+    return<>
+    <div>
+    <p className='text-black text-xl'>sdsds</p>
+    </div>
+    <SvgAnimator/>
+    </>
+}

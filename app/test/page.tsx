5cdc8e12f0beb2a99a0979ae82371012156f4154
Vlad <vlad.chk@gmail.com>
2025-05-17 12:21:43 -0300

    fix: ups!!

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,26 @@
+'use client'
 import LdBanner from '@/components/custom/ld-banner-convocatoria'
+import { LdSvg } from '@/components/custom/ld-svg'
+import CajaTexto from '@/svg/CajaTextoPre.svg'
 
 export default function Page() {
+  const one = <p>Tuvieja</p>
+
   return (
     <div className="h-screen w-4/5 mx-auto">
       <LdBanner />
+
+      <LdSvg
+        className="text-2xl w-full"
+        SvgComponent={CajaTexto}
+        ids={['cajaUno', 'cajaDos'] as const}
+        animation={(t, nodos) => {
+          const i = 500
+          nodos['cajaUno'].attr({ opacity: t % i < i / 2 ? 1 : 0 })
+          nodos['cajaDos'].attr({ opacity: t % i > i / 2 ? 1 : 0 })
+        }}
+        slots={{ slotCaja: one } as const}
+      />
     </div>
   )
 }

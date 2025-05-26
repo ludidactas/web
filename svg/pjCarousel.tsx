7b02acba627640e48c0b376a2fa2797af774722e
Vlad <vlad.chk@gmail.com>
2025-05-26 00:15:25 -0300

    dev: personajes del carousel

---
+++ svg/pjCarousel.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { LdSvg } from '@/components/custom/ld-svg'
+import { oscilar } from '@/lib/utils'
+import { PropsWithChildren } from 'react'
+
+import Biclope from '@/svg/dist/identidad/biclope.svg'
+import Ojito from '@/svg/dist/identidad/ojito.svg'
+import Pulpo from '@/svg/dist/identidad/pulpo.svg'
+import Robot from '@/svg/dist/identidad/robot.svg'
+
+const personajes = {
+  biclope: Biclope,
+  ojito: Ojito,
+  pulpo: Pulpo,
+  robot: Robot,
+}
+
+interface Props extends PropsWithChildren {
+  personaje: keyof typeof personajes
+}
+
+export default function PjCarousel({ personaje, children }: Props) {
+  return (
+    <LdSvg
+      SvgComponent={personajes[personaje]}
+      ids={['personaje', 'items', 'bg', 'slot']}
+      slots={{ slot: children }}
+      animation={oscilar(['items', 'bg', 'personaje'], 0.5, 0.08, 1)}
+    />
+  )
+}

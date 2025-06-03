787a5962a6627ccb568fe5fd12c6893b13d40dca
Aleja <vlad.chk@gmail.com>
2025-06-03 09:16:08 -0500

    Intento arreglo arrow down :3

--- components/custom/ld-arrowDown.tsx
+++ components/custom/ld-arrowDown.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-scroll";
 import { LdSvg } from "./ld-svg";
-import ArrowDown from "@/svg/arrowDown.svg"
+import ArrowDown from "@/svg/arrowDownSVGO.svg"
+import { secuenciar } from "@/lib/utils";
 
 interface ArrowDownLdProps{
     to:string
@@ -9,14 +10,13 @@ export default function ArrowDownLd({ to }: ArrowDownLdProps){
     return(
       <div className="w-10 lg:w-16 hover:scale-125 ">
              <Link to={to} smooth={true} duration={500}>
-                <LdSvg
-                SvgComponent={ArrowDown}
-                ids={['uno','dos','tres']as const}
-                animation={(nodos, t) => {
-                  const i = 500
-                  nodos['uno'].attr({ opacity: t % i < i / 2 ? 1 : 0 })
-                  nodos['dos'].attr({ opacity: t % i > i / 2 ? 1 : 0 })
-                  nodos['tres'].attr({ opacity: t % i < i / 4 ? 1 : 0 })                }}/>
+                  <LdSvg className='text-2xl m-4 w-full'
+              SvgComponent={ArrowDown}
+              ids={["uno", "dos", "tres"] as const}
+              animation={secuenciar(["uno", "dos", "tres"], 700)}
+              
+
+            />
                {/* <Image 
                  src="/img/ArrowDown.gif" 
                  className="[animation:bounce_0.8s_infinite] hover:rounded-full hover:border-2 hover:border-black " 

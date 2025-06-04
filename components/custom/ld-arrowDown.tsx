6702b337796ff11f2403b024a5df349a7193e770
Aleja <vlad.chk@gmail.com>
2025-06-04 00:19:07 -0500

    dev: volvimos al arrow down que funcionaba

--- components/custom/ld-arrowDown.tsx
+++ components/custom/ld-arrowDown.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-scroll";
-import { LdSvg } from "./ld-svg";
-import ArrowDown from "@/svg/arrowDownSVGO.svg"
-import { secuenciar } from "@/lib/utils";
+// import { LdSvg } from "./ld-svg";
+// import ArrowDown from "@/svg/arrowDownSVGO.svg"
+// import { secuenciar } from "@/lib/utils";
+import Image from "next/image";
 
 interface ArrowDownLdProps{
     to:string
@@ -10,20 +11,20 @@ export default function ArrowDownLd({ to }: ArrowDownLdProps){
     return(
       <div className="w-10 lg:w-16 hover:scale-125 ">
              <Link to={to} smooth={true} duration={500}>
-                  <LdSvg className='text-2xl m-4 w-full'
+                  {/* <LdSvg className='text-2xl m-4 w-full'
               SvgComponent={ArrowDown}
               ids={["uno", "dos", "tres"] as const}
               animation={secuenciar(["uno", "dos", "tres"], 700)}
               
 
-            />
-               {/* <Image 
+            /> */}
+               <Image 
                  src="/img/ArrowDown.gif" 
-                 className="[animation:bounce_0.8s_infinite] hover:rounded-full hover:border-2 hover:border-black " 
+                 className=" hover:rounded-full hover:border-2 hover:border-black " 
                  alt="arrowdown" 
                  width={200} 
                  height={200} 
-               /> */}
+               />
              </Link>
            </div>
          )

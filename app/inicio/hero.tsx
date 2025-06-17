807a5e5d262843108c692aaa77c98510eb2b2c5f
Vlad <vlad.chk@gmail.com>
2025-06-17 14:54:11 -0300

    dev: versión mobile y paso ambos al hero

--- app/inicio/hero.tsx
+++ app/inicio/hero.tsx
@@ -1,14 +1,18 @@
 'use client'
 
-import LdBannerConvocatoria from '@/components/custom/ld-banner-convocatoria'
-import LdBannerConvocatoriaMobile from '@/components/custom/ld-banner-convocatoria-mobile'
+import LdBannerVCabrera from '@/components/custom/ld-banner-vcabrera'
+// Banners de la convocatoria a talleristas que no fue
+// import LdBannerConvocatoria from '@/components/custom/ld-banner-convocatoria'
+// import LdBannerConvocatoriaMobile from '@/components/custom/ld-banner-convocatoria-mobile'
+
+import LdBannerVCabreraMobile from '@/components/custom/ld-banner-vcabrera-mobile'
 import { CircleChevronDown } from 'lucide-react'
 import { Link as Scroll } from 'react-scroll'
 
 const HeroMobile = () => {
   return (
     <>
-      <LdBannerConvocatoriaMobile />
+      <LdBannerVCabreraMobile />
 
       <div className="w-[8vw] my-10">
         <Scroll to="portadaini" smooth={true} duration={500}>
@@ -26,7 +30,7 @@ const Hero = () => {
         <HeroMobile />
       </div>
       <div className="hidden lg:block">
-        <LdBannerConvocatoria className="w-[80vw]" />
+        <LdBannerVCabrera className="w-[80vw]" />
       </div>
     </>
   )
---
+++ components/custom/ld-banner-vcabrera-mobile.tsx
@@ -0,0 +1,38 @@
+'use client'
+import ConvocatoriaSvg from '@/svg/dist/bannervcabreramobile.svg'
+import BtnSketchy2 from './ld-btn-sketchy-2'
+import { LdSvg } from './ld-svg'
+
+export default function LdBannerVCabreraMobile({ className }: { className?: string }) {
+  const boton = (
+    <div className="w-full h-full flex items-center justify-center">
+      <BtnSketchy2 href="https://www.instagram.com/ludidactas" className='h-4/5'>
+        <p className="text-center text-xl">Inscripción</p>
+      </BtnSketchy2>
+    </div>
+  )
+
+  return (
+    <LdSvg
+      // El svg importado
+      SvgComponent={ConvocatoriaSvg}
+      // Los ids que le hayamos puesto a los elementos
+      ids={['mano', 'personajes', 'info', 'fondo', 'slot'] as const}
+      // Los slots que hayamos dejado, mapeando a su contenido
+      slots={{ slot: boton } as const}
+      // Función setup
+      setup={(nodos) => {
+        // Le aplicamos el blend mode al cono
+        nodos['mano'].node.style.mixBlendMode = 'divide'
+      }}
+      // Función loop
+      animation={(nodos, t) => {
+        ['mano', 'personajes', 'info', 'fondo', 'slot'].forEach((id, idx) => {
+          const nodo = nodos[id]
+          nodo.dy(Math.sin(t / 600 + idx) * 0.04)
+        })
+      }}
+      className={`${className ?? ''}`}
+    />
+  )
+}

7338791a25892e0c77da239a96cf06a7ab3a6324
Vlad <vlad.chk@gmail.com>
2025-06-18 10:57:40 -0300

    fix: layot portada

--- app/inicio/portada.tsx
+++ app/inicio/portada.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Portada = () => {
   return (
-    <div className="portadaini w-[100vw] h-[100vh] mt-20 flex flex-col items-center justify-center mb-20">
+    <div className="portadaini w-[100vw] h-[100vh] mt-20 flex flex-col gap-8 items-center justify-center mb-20">
       <Image className="aspect-auto object-fill" src="/img/Compo.png" alt="PortadaImg" width={1000} height={1000} />
 
       <h3 className={`${titulo.className} text-2xl lg:text-3xl text-center lg:mx-80 lg:mb-20`}>

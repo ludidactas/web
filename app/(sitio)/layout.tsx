f18a2c6ec449613ba92eaa4d1924cf498e2542fb
Vlad <vlad.chk@gmail.com>
2025-07-18 18:29:38 -0300

    refactor: sitio dentro de un segmento dedicado

---
+++ app/(sitio)/layout.tsx
@@ -0,0 +1,22 @@
+import Textura from '@/components/fx/textura'
+import LogoLema from './inicio/logoLema'
+import { Menu } from 'lucide-react'
+import Footer from './inicio/footer'
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <Textura>
+      {/* Barra top */}
+      <div className="flex dark:bg-[#1e1e1e] justify-between items-center p-2 px-4">
+        <LogoLema />
+        <Menu />
+      </div>
+      {/* Cajita punteada exterior */}
+      <div className="mx-2 lg:mx-10 border-x-4 border-slate-200 border-dashed flex flex-col items-center ">
+        {children}
+      </div>
+
+      <Footer />
+    </Textura>
+  )
+}

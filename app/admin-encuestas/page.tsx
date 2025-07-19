90a990095077b837cd43f931cf06e4df1cfd8557
Vlad <vlad.chk@gmail.com>
2025-07-19 00:04:25 -0300

    dev: mensajes del server con toast

--- app/admin-encuestas/page.tsx
+++ app/admin-encuestas/page.tsx
@@ -2,10 +2,12 @@
 
 import EncuestasAdmin from '@/app/admin-encuestas/componentes/encuestas-admin'
 import { EncuestaProvider } from '@/app/admin-encuestas/componentes/encuestas-context'
+import { Toaster } from '@/components/ui/sonner'
 
 export default function Page() {
   return (
     <EncuestaProvider>
+      <Toaster/>
       <div className="min-h-screen w-screen mx-auto flex flex-col gap-8 items-center">
         <div className="p-8 w-4/5">
           <EncuestasAdmin />

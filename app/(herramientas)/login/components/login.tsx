7d261c1c0b136d2fc1158df28ffa2601164c8033
Alejandra <comomaraleja@gmail.com>
2025-08-06 20:11:14 -0300

    Se borro cosito

--- app/(herramientas)/login/components/login.tsx
+++ app/(herramientas)/login/components/login.tsx
@@ -9,10 +9,6 @@ export default function Login() {
 
             <Image className="rounded-full shadow-xl" width={400} height={400} src={"/img/ilustracionLogin.png"} alt={""} />
             <div className="flex flex-col h-fit rounded-xl items-center">
-                {/* <div className="flex gap-4 items-center">
-                    <Image className="h-14 w-14" width={50} height={100} src={"/img/Logo.png"} alt={"Logo"} />
-                    <Image className='h-10 w-100' width={300} height={50} src={"/img/Lema.png"} alt={"Nombre"} />
-                </div> */}
                 <div className="flex md:w-[30em] items-center gap-4">
                                   <Image className="w-8 md:w-16" src="/img/logo_sketchy.gif" alt={''} width={100} height={100} />
                                   <div className="font-medium text-[7px] sm:text-[12px] md:text-[14px] lg:text-[18px] pt-1">

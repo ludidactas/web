29e32a668480c08e052bc917770aa52c26bad877
Vlad <vlad.chk@gmail.com>
2025-08-30 16:42:59 -0300

    dummy

--- app/(herramientas)/encuestas/ping/page.tsx
+++ app/(herramientas)/encuestas/ping/page.tsx
@@ -2,7 +2,6 @@ import { SessionProvider } from "next-auth/react";
 import TestClient from "./cliente";
 
 export default async function PingPage() {
-
   return (
     <SessionProvider>
       <TestClient />

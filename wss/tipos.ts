77c769f5e93835a41b22138cb3bc71ffe3fe4750
Vlad <vlad.chk@gmail.com>
2025-10-22 11:56:47 -0300

    dev: agrego `admiteAportes` a la interfaz de encuesta

--- wss/tipos.ts
+++ wss/tipos.ts
@@ -4,11 +4,18 @@ export interface Encuesta {
   id: string
   pregunta: string
   opciones: Opcion[]
+  /** Fecha y hora de creación */
   createdAt: string
+  /** Define si está abierta, es decir si recibe votos */
   isOpen: boolean
+  /** Define si está publicada, es decir si es visible para lxs estudiantes */
   isPublished: boolean
+  /** Define se está enfocada en el overlay */
   isFocused: boolean
+  /** Define si los votos y las opciones son visibles en el overlay y la vista de estudiante */
   isRevealed: boolean
+  /** Define si una encuesta puede recibir respuestas adicionales a las opciones dadas */
+  admiteAportes: boolean
 }
 
 export interface Opcion {

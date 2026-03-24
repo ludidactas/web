5c433f9175a4a75eaacf9c94c68cec9624b7090e
Vlad <vlad.chk@gmail.com>
2026-03-24 01:10:22 -0300

    Height más flexible para secciones de landing

--- app/(sitio)/inicio/pantalla.tsx
+++ app/(sitio)/inicio/pantalla.tsx
@@ -5,87 +5,74 @@ import { cn } from '@/lib/utils'
 // import CajaTexto from '@/svg/CajaPrueba3SVGO.svg'
 
 export interface PantallaProps {
-  title?: React.ReactNode,
-  one: JSX.Element,
-  two: JSX.Element,
-  btn: JSX.Element,
-  scroll: JSX.Element,
-  espejado?: boolean,
-  classname?: string,
-
+  title?: React.ReactNode
+  one: JSX.Element
+  two: JSX.Element
+  btn: JSX.Element
+  scroll: JSX.Element
+  espejado?: boolean
+  classname?: string
 }
 
 const Pantalla = ({ title, one, two, btn, scroll, espejado = false, classname }: PantallaProps) => {
-  return (<>
+  return (
+    <>
+      {/* Shape divider top */}
+      <div className={cn('shape-divider-waves h-[90px] w-full text-white')} />
 
-    {/* Shape divider top */}
-    <div className={cn('shape-divider-waves h-[90px] w-full text-white')} />
+      <div className={`w-[100vw] min-h-[100vh] flex-col items-center place-content-center mb-20 ${classname}`}>
+        {/* DESKTOP */}
+        <div className="hidden lg:flex flex-col items-center my-10 ">
+          <h2 className={`${titulo.className} text-7xl mb-20 drop-shadow-[4px_4px_2px_rgba(20,20,20)]`}>{title}</h2>
 
-    <div className={`w-[100vw] h-[100vh] flex-col items-center place-content-center mb-20 ${classname}`}>
+          <div className={`flex items-center gap-20 ${espejado ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
+            <div
+              className=" flex flex-col w-fit gap-8 text-left bg-white/60 p-10 rounded-xl"
+              data-aos={espejado ? 'fade-left' : 'fade-right'}
+            >
+              {one}
+              {btn}
+            </div>
 
-      {/* DESKTOP */}
-      <div className="hidden lg:flex flex-col items-center my-10 ">
-        <h2
-          className={`${titulo.className} text-7xl mb-20 drop-shadow-[4px_4px_2px_rgba(20,20,20)]`}
-        >
-          {title}
-        </h2>
-
-        <div className={`flex items-center gap-20 ${espejado ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
-
-          <div
-            className=" flex flex-col w-fit gap-8 text-left bg-white/60 p-10 rounded-xl"
-            data-aos={espejado ? 'fade-left' : 'fade-right'}
-          >
-            {one}
-            {btn}
-
+            <div className="w-[25vw]" data-aos={espejado ? 'fade-right' : 'fade-left'}>
+              {two}
+            </div>
           </div>
 
-          <div className="w-[25vw]" data-aos={espejado ? 'fade-right' : 'fade-left'}>
-            {two}
-          </div>
+          <div className="mt-10 text-center">{scroll}</div>
         </div>
 
-        <div className="mt-10 text-center">{scroll}</div>
-      </div>
+        {/* MOBILE */}
 
-      {/* MOBILE */}
+        <div className="flex lg:hidden flex-col place-content-center items-center w-full h-full my-20 pb-10">
+          <div className="flex flex-col mx-8 items-center text-[0.5em] text-center p-4">
+            <h2
+              data-aos="fade-down"
+              className={`${titulo.className} text-4xl drop-shadow-[2px_2px_2px_rgba(0,0,0)] bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text`}
+            >
+              {title}
+            </h2>
 
-      <div className="flex lg:hidden flex-col place-content-center items-center w-full h-full my-20 pb-10">
-        <div className="flex flex-col mx-8 items-center text-[0.5em] text-center p-4">
-          <h2
-            data-aos="fade-down"
-            className={`${titulo.className} text-4xl drop-shadow-[2px_2px_2px_rgba(0,0,0)] bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text`}
-          >
-            {title}
-          </h2>
+            <div className="w-[35vw] m-4" data-aos="fade-left">
+              {two}
+            </div>
 
-
-          <div className="w-[35vw] m-4" data-aos="fade-left">
-            {two}
+            <div
+              className="w-full self-center text-center p-2 rounded-xl bg-white/50"
+              data-aos={espejado ? 'fade-left' : 'fade-right'}
+            >
+              {one}
+              {btn}
+            </div>
           </div>
 
-          <div
-            className="w-full self-center text-center p-2 rounded-xl bg-white/50"
-            data-aos={espejado ? 'fade-left' : 'fade-right'}
-          >
-            {one}
-            {btn}
-
-          </div>
-        </div>
-
-        <div className="flex justify-center w-40 md:m-10 mb-10">
-          {scroll}
+          <div className="flex justify-center w-40 md:m-10 mb-10">{scroll}</div>
         </div>
       </div>
-    </div>
 
-    {/* Shape divider bottom */}
-    <div className={cn('shape-divider-waves-bottom h-[90px] w-full text-white')} />
-
-  </>
+      {/* Shape divider bottom */}
+      <div className={cn('shape-divider-waves-bottom h-[90px] w-full text-white')} />
+    </>
   )
 }
 export default Pantalla

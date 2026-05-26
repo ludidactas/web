00385f3fa5ebed2af8dfede598b07f0abc1602f2
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2026-05-26 19:59:24 -0300

    fix: actualización de santización

--- wss/salas/handlers.ts
+++ wss/salas/handlers.ts
@@ -29,7 +29,7 @@ export const handlersSalaProfe = async (socket: SocketProfe) => {
       await sala.sanitizar()
 
       // También revocamos las sesiones de los estudiantes que no estén en la lista de permitidos (si es que la sala tiene lista de permitidos)
-      if ((await sala.listaPermitidos().obtener()).length > 0) {
+      if ((await sala.config().solo_invitados) {
         await sala.sanitizarPermitidos()
       }
       

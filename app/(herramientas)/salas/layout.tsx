c1b0f416e80352ac7178ac4f26031bd6d4c350ed
Alejandra <comomaraleja@gmail.com>
2026-06-24 16:50:17 -0300

    Se convirtió SalaLayout de un componente client a un Server Component async

--- app/(herramientas)/salas/layout.tsx
+++ app/(herramientas)/salas/layout.tsx
@@ -1,10 +1,14 @@
+import { auth } from '@/app/auth'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { SessionProvider } from 'next-auth/react'
 
-export default function SalaLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function SalaLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth()
   return (
-    <SessionProvider>
-      <TooltipProvider>{children}</TooltipProvider>
+    <SessionProvider session={session}>
+      <TooltipProvider>
+        {children}
+      </TooltipProvider>
     </SessionProvider>
   )
 }

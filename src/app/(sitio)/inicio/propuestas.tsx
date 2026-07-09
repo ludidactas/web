42ed93d7084962175ef44f23a8514de6ddac8b9b
Vlad <vlad.chk@gmail.com>
2026-07-09 01:03:34 -0300

    Cambio de ubicación de secciones en portada y algunos fraseos

--- src/app/(sitio)/inicio/propuestas.tsx
+++ src/app/(sitio)/inicio/propuestas.tsx
@@ -1,23 +1,21 @@
 'use client'
 
-import Image from 'next/image'
-import Pantalla from './pantalla'
-import { Hl } from './highlight'
 import ArrowDownLd from '@/components/custom/ld-arrow-down'
 import { BotonLink } from '@/components/custom/ld-boton-svg'
 import { Title } from '@/components/custom/ld-title'
-
+import Image from 'next/image'
+import Pantalla from './pantalla'
 
 export default function Propuestas() {
   return (
-    <div className="propuestasini">
+    <div className="propuestasini bg-indigo-300/50">
       <Pantalla
-        title={<Title radius={2} text={'Propuestas'} color={'text-[#00B0D2]'} size={'text-4xl md:text-7xl'}/>}
+        title={<Title radius={2} text={'Propuestas'} color={'text-[#8345FD]'} size={'text-4xl md:text-7xl'} />}
         one={
-          <div className="flex flex-col md:max-w-[45vw] items-center gap-2 text-[1.4em] md:text-[1em]">
+          <div className="flex flex-col md:max-w-[45vw] items-center gap-2 text-sm md:text-[1em]">
             <p>
               El proyecto nace, vive y se sostiene en su comunidad y se ordena en torno la visión de construir la
-              educación que queremos ver.
+              educación que queremos ver en el mundo.
             </p>
             <p>
               Si buscás acercarte al rol de profe o si ya lo estás practicando, encontrarás pares que están aprendiendo
@@ -35,17 +33,18 @@ export default function Propuestas() {
             </p>
 
             <p>
-              Organizamos el contenido y propuestas en tres líneas: <Hl>técnica, didáctica y pedagógica. </Hl>
+              De momento nos estamos volcando enteramente al desarrollo de herramientas. Pronto volveremos a abrir las
+              propuestas formativas tanto para estudiantes como formadores.
             </p>
+
+            {/* <p>
+              Organizamos el contenido y propuestas en tres líneas: <Hl>técnica, didáctica y pedagógica. </Hl>
+            </p> */}
           </div>
         }
         two={<Imagenes />}
-        btn={
-          <BotonLink titulo={'Proximamente...'} url={''} />
-
-        }
+        btn={<BotonLink titulo={'Proximamente...'} url={''} />}
         scroll={<ArrowDownLd to="recursosini" />}
-        espejado={true}
       />
     </div>
   )

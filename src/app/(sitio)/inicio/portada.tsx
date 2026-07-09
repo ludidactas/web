d774775d7578a8f5eb6841d1d59103f3aa06b989
Vlad <vlad.chk@gmail.com>
2026-07-09 11:59:44 -0300

    Fix de layout

--- src/app/(sitio)/inicio/portada.tsx
+++ src/app/(sitio)/inicio/portada.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 const Portada = () => {
   return (
     <>
-      <div className="portadaini h-[calc(100vh-256px)] flex flex-col items-center md:justify-between mb-20 mt-20 md:mt-0">
+      <div className="portadaini min-h-[calc(100vh-256px)] flex flex-col items-center md:justify-between mb-20 mt-20 md:mt-0">
         <Image className="w-[80vw] md:w-[45vw]" src="/img/Compo.webp" alt="PortadaImg" width={1000} height={1000} />
         <h3 className={`${titulo.className} text-xl mb-20 mx-10 md:text-2xl lg:text-3xl text-center lg:mx-80 lg:mb-10`}>
           <p>

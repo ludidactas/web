ab678ac4b620406f1257c3a00986c7eb82850547
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2026-07-28 16:54:50 -0300

    ui: fix chiquito de overflow de texto en login

--- src/app/(herramientas)/login/components/ilustracion.tsx
+++ src/app/(herramientas)/login/components/ilustracion.tsx
@@ -6,7 +6,7 @@ import { oscilar } from '@/lib/animaciones'
 export default function Ilustracion() {
   return (
     <LdSvg
-      className="w-[500px] md:w-[600px] drop-shadow-xl"
+      className="w-full max-w-[500px] md:w-[600px] drop-shadow-xl"
       SvgComponent={ilustracionProfe}
       ids={['uno', 'dos', 'tres', 'cuatro'] as const}
       animation={oscilar(['uno', 'cuatro', 'tres'], 2, 0.2, 0.2)}

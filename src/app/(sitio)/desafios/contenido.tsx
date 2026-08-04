2f6288ec6a6eebcef0015a00c9fd000c29aa1a16
Vlad <vlad.chk@gmail.com>
2026-08-04 13:16:38 -0300

    Desafíos de scratch mobile-friendly

--- src/app/(sitio)/desafios/contenido.tsx
+++ src/app/(sitio)/desafios/contenido.tsx
@@ -10,20 +10,32 @@ export default function ContenidoDesafios({ desafios }: { desafios: Desafio[] })
     <>
       <div className="flex flex-col w-full min-h-screen bg-indigo-300">
         <ShapeDividerWaves bottom colorText={'text-white'} />
-        <div className='flex my-10 px-20 gap-6'>
-        <Image width={300} height={100} src={'/desafios/JornadasLogo.png'} alt={''}/>
-        <Title className="" text="Desafíos 24° Jornadas de Recreación" color="text-[#8b5cf6]" size="text-5xl md:text-7xl" />
+        <div className="flex my-10 px-2 md:px-20 gap-6">
+          <Image
+            className="w-24 h-auto md:w-[300px]"
+            width={583}
+            height={356}
+            src={'/desafios/JornadasLogo.png'}
+            alt={''}
+          />
+          <Title
+            className=""
+            text="Desafíos 24° Jornadas de Recreación"
+            color="text-[#8b5cf6]"
+            size="text-2xl md:text-5xl md:text-7xl"
+          />
         </div>
-        <div className='flex flex-col gap-4 mx-40 p-6 text-xl bg-white/50 rounded-xl'>
-          <p className=''>
-            Hemos construido una serie de desafíos para que puedan explorar y familiarizarse con Scratch.
-            Los desafíos consisten en pequeños artefactos jugables que no funcionan como se esperaria.
-            Para hacerlos funcionar, tendran disponibles algunas pistas y la solución de cada uno. 
-            No olviden conversar las posibles soluciones y hacer preguntas: aprender es una actividad colectiva</p>
-          <p className='text-4xl text-[#8b5cf6] text-center'> ¡A explorar!</p>
+        <div className="flex flex-col gap-4 mx-4 md:mx-40 p-6 text-xl bg-white/50 rounded-xl">
+          <p className="">
+            Hemos construido una serie de desafíos para que puedan explorar y familiarizarse con Scratch. Los desafíos
+            consisten en pequeños artefactos jugables que no funcionan como se esperaria. Para hacerlos funcionar,
+            tendran disponibles algunas pistas y la solución de cada uno. No olviden conversar las posibles soluciones y
+            hacer preguntas: aprender es una actividad colectiva
+          </p>
+          <p className="text-4xl text-[#8b5cf6] text-center"> ¡A explorar!</p>
         </div>
 
-        <div className="flex flex-col items-center px-6 py-10 gap-2">
+        <div className="flex flex-col items-center px-6 py-10 gap-16 md:gap-2">
           {desafios.map((d) => (
             <DesafioCard key={d.numero} {...d} />
           ))}
